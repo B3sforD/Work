1d09b1d740867e50a615c22cdb51b0e3f537b316
B3sforD <vbif_2017@bk.ru>
2024-05-23 18:41:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+#Длина слова
+a = 'true'
+print(len(a))
+
+#Суммы и разности
+first = 2
+second = 3
+
+summa = first + second
+diff = first - second
+
+print(summa, diff)
+
+#среднее арифметическое
+first = 10
+second = 3
+third = 6
+
+b = first + second + third
+
+mean = b / 3
+
+print(mean)
+
+#Простые строчки
+
+first_string = "Вторник"
+
+second_string ="Понедельник"
+
+print(second_string +", " +first_string)
+
+#Сложная формула
+
+a = 5
+b = 6
+c = 7
+
+f = ((a * b) + (a * c)) ** 3
+
+print(f / 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
