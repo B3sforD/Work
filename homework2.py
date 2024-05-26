19f5398da5360cd5e8db4a0dfebbb17243651d93
B3sforD <vbif_2017@bk.ru>
2024-05-26 12:36:31 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+Number_of_homework = 12
+
+Number_of_hours_spent = 1.5
+
+CourseName = "Python"
+
+Time_per_task = Number_of_hours_spent / Number_of_homework
+
+print('Курс: ' + CourseName + ", всего задач:" + str(Number_of_homework) + ', затрачено часов:' + str(Number_of_hours_spent) + ', среднее время выполнения:' + str(Time_per_task) +'часа.')
